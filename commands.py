b2576cda3d3cb4deb9fedceb98aaa700c70905d3
Moyeong Lee <Moyeong.Lee@Bain.com>
2019-10-18 11:43:03 -0700

    - Complete commands module - Complete the first version of miner

--- commands.py
+++ commands.py
@@ -1,16 +1,31 @@
-import json
 
 def locate(companies, arg):
-  filtered = filter(lambda company: company.state == arg, companies.items())
-  print('filtered', dict(filtered))
-  # mapped = map(lambda : expression)
+  # for company in companies:
+  #   print('company state', company['state'])
+  # filtered = [x for x in companies if x['state'] == arg]
+  filtered_companies = list(filter(lambda company: company['state'] == arg, companies))
+  result_companies = list(map(lambda company: company['company_name'], filtered_companies))
+  return result_companies
+
+def find_before(companies, arg):
+  filtered_companies = list(filter(lambda company: company['year_founded'] and company['year_founded'] <= int(arg), companies))
+  result_companies = list(map(lambda company: company['company_name'], filtered_companies))
+  return result_companies
 
 
-# def find_before():
-#   lambda company_data, arg: company_names,
-# def find_after(): 
-#   lambda,
-# def find_companies_between_size(): 
-#   lambda,
-# def find_type(): 
-#   lambda
+def find_after(companies, arg): 
+  filtered_companies = list(filter(lambda company: company['year_founded'] and company['year_founded'] >= int(arg), companies))
+  result_companies = list(map(lambda company: company['company_name'], filtered_companies))
+  return result_companies
+
+
+def find_companies_between_size(companies, arg): 
+  filtered_companies = list(filter(lambda company: company['full_time_employees'] == arg, companies))
+  result_companies = list(map(lambda company: company['company_name'], filtered_companies))
+  return result_companies
+
+
+def find_type(companies, arg): 
+  filtered_companies = list(filter(lambda company: company['company_category'] == arg, companies))
+  result_companies = list(map(lambda company: company['company_name'], filtered_companies))
+  return result_companies
--- miner.py
+++ miner.py
@@ -8,11 +8,16 @@ def miner():
   with open(file=sys.argv[1], mode='r') as file:
     try: 
       companies_data = json.load(file)
-      # print(json.dumps(data, indent=4))
-      getattr(commands, sys.argv[2])(companies_data, sys.argv[3])
+      result = getattr(commands, sys.argv[2])(companies_data, sys.argv[3])
+      logResult(result)
     except IOError as e:
       print('IOError: ', e)
     except:
-      print('Unexpected error: ', sys.exc_info()[0])
-  
+        print('Unexpected error: ', sys.exc_info()[0])
+
+def logResult(result):
+  company_names = ', '.join(result)
+  company_numbers = len(result)
+  print(f'Company names:\n{company_names}\n\nNumber of Companies: {company_numbers}')
+
 miner()
